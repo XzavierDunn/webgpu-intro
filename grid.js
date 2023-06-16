85bfb816178d79d37e218db5fae344d0ae9d16f8
Xzavier Dunn <xmandunn@gmail.com>
2023-06-15 19:57:33 -0700

    finished grid

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 ///////// GENERAL SETUP
-const canvas = document.getElementById("intro");
+const canvas = document.getElementById("grid");
 if (!navigator.gpu) {
     throw new Error("WebGPU not supported on this browser.");
 };
@@ -32,6 +32,33 @@ const pass = encoder.beginRenderPass({
     }]
 });
 ///////// SETUP INTERFACE TO RECORD GPU COMMANDS
+
+///////// WGSL
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: `
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f, @builtin(instance_index) instance: u32) -> @builtin(position) vec4f {
+
+        let i = f32(instance);
+      
+        let cell = vec2f(i % grid.x, floor(i / grid.x));
+
+        let cellOffset = cell / grid * 2;
+        let gridPos = (pos + 1) / grid - 1 + cellOffset;
+
+        return vec4f(gridPos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(1, 0.7, 0.7, 1);
+    }
+    `
+});
+///////// WGSL
 
 ///////// INNER SQUARE SETUP
 const vertices = new Float32Array([
@@ -85,21 +112,6 @@ const vertexBufferLayout = {
     }],
 };
 
-const cellShaderModule = device.createShaderModule({
-    label: "Cell shader",
-    code: `
-    @vertex
-    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
-        return vec4f(pos, 0, 1);
-    }
-
-    @fragment
-    fn fragmentMain() -> @location(0) vec4f {
-        return vec4f(1, 0.7, 0.7, 1);
-    }
-    `
-});
-
 const cellPipeline = device.createRenderPipeline({
     label: "Cell pipeline",
     layout: "auto",
@@ -119,10 +131,40 @@ const cellPipeline = device.createRenderPipeline({
 
 pass.setPipeline(cellPipeline);
 pass.setVertexBuffer(0, vertexBuffer);
-pass.draw(vertices.length / 2);
 ///////// INNER SQUARE SETUP
 
+
+///////// GRID SETUP
+const GRID_SIZE = 6;
+
+// Uniform buffer
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+const uniformBuffer = device.createBuffer({
+    label: "Grid Uniforms",
+    size: uniformArray.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+const bindGroup = device.createBindGroup({
+    label: "Cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+        binding: 0,
+        resource: { buffer: uniformBuffer }
+    }],
+});
+
+// each binding that is part of group 0 uses the resources in this bind group
+pass.setBindGroup(0, bindGroup);
+
+///////// GRID SETUP
+
+
 ///////// Send Commands
+
+pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
+
 // end render pass
 pass.end();
 
